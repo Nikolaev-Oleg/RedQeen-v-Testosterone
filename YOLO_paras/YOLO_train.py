08cac301fbfd4485b27e878bfd4eb32fddea19fd
Nikolaev-Oleg <121944350+Nikolaev-Oleg@users.noreply.github.com>
2026-06-20 14:01:52 +0300

    Update and rename YOLO2.2.py to YOLO_paras/YOLO_train.py

--- YOLO_paras/YOLO_train.py
+++ YOLO_paras/YOLO_train.py
@@ -3,5 +3,5 @@ import os
 import cv2
 from ultralytics import YOLO
 
-model = YOLO("../runs/detect/train6/weights/last.pt")
+model = YOLO("yolov10s.pt")
 results = model.train(data='data.yaml', epochs=500, imgsz=640, plots=True, save_period=10)
