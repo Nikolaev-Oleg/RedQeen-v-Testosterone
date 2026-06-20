dc572e759ac935a7a0d370e921beb5074b6fe413
Nikolaev-Oleg <121944350+Nikolaev-Oleg@users.noreply.github.com>
2026-06-20 13:19:53 +0300

    Update YOLO_RBCcounter_1.2.py

--- YOLO_RBC/YOLO_RBCcounter_1.2.py
+++ YOLO_RBC/YOLO_RBCcounter_1.2.py
@@ -5,40 +5,41 @@ from ultralytics import YOLO
 import pandas as pd
 import shutil
 
-model = YOLO("./runs_Egor/runs/detect/train5/weights/best.pt")
-root_dir = 'C:/Users/nikol/OneDrive/Изображения/Пленка'
-dirs = [f'{root_dir}/{d.name}' for d in os.scandir(root_dir) if d.is_dir()]
-cl = [0, 12, 17]
+model = YOLO("./best.pt") # weights
+root_dir = 'root/directory' # path to directory containing subdirectories with images
+dirs = [f'{root_dir}/{d.name}' for d in os.scandir(root_dir) if d.is_dir()] # list of subdirectories
+cl = [0, 12, 17] # list of classes to count (RBC, hcRBC, lysRBC)
 
-paras_df = pd.DataFrame(columns=['id', 'RBC', 'hcRBC', 'lysRBC'])
+paras_df = pd.DataFrame(columns=['id', 'RBC', 'hcRBC', 'lysRBC']) # initiate dataframe
 for d in dirs:
     nfiles = len([f for f in os.listdir(d)])
     if nfiles > 0:
         results = model.predict(d,
-                                save=True,
-                                classes=cl,
+                                save=True, # draw frames
+                                classes=cl, # search for classes in cl
                                 agnostic_nms=True,
                                 project=root_dir,
-                                name='temp',
+                                name='temp', # temporary directory
                                 line_width=2,
-                                conf=0.56)
+                                conf=0.56) # confidence level maximizing F1
         if os.path.exists(f'{d}/YOLO1.5b'):
-            shutil.rmtree(f'{d}/YOLO1.5b')
+            shutil.rmtree(f'{d}/YOLO1.5b') # overwrite directory with frames
         os.rename(f'{root_dir}/temp', f'{d}/YOLO1.5b')
 
+        # Calculate cells
         s = 0
         for i in results:
-            s = s + len(i.boxes.cls[i.boxes.cls == 0])
+            s = s + len(i.boxes.cls[i.boxes.cls == 0]) # RBC
         print('RBC:' + str(s))
 
         s1 = 0
         for i in results:
-            s1 = s1 + len(i.boxes.cls[i.boxes.cls == 12])
+            s1 = s1 + len(i.boxes.cls[i.boxes.cls == 12]) # hcRBC
         print('hcRBC:' + str(s1))
 
         s2 = 0
         for i in results:
-            s2 = s2 + len(i.boxes.cls[i.boxes.cls == 17])
+            s2 = s2 + len(i.boxes.cls[i.boxes.cls == 17]) #lysRBC
         print('lysRBC:' + str(s2))
 
         name = d.replace(f'{root_dir}/', '')
@@ -46,4 +47,4 @@ for d in dirs:
 
         paras_df.loc[len(paras_df)] = newrow
         paras_df.to_csv('./YOLO1.5b_RBC_main2.csv',
-                          index=False)
+                          index=False) # write to .csv
